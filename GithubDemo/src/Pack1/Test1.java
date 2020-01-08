efd0342879d83753c14af884f301e70972fd5ec1
Prakash <Prakash@DESKTOP-FBT3I7F>
2020-01-08 11:27:40 +0530

    this is my first commit

---
+++ GithubDemo/src/Pack1/Test1.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void case1()
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void case2()
+	{
+		System.out.println("world");
+	}
+
+}
